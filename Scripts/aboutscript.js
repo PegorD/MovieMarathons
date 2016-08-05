ab2afcf16ac75b024963bd2c01cf3c6b4443328d
Knowledgeable Mammoth <T-K-M@users.noreply.github.com>
2016-08-05 11:27:50 +0300

    Update aboutscript.js

--- Scripts/aboutscript.js
+++ Scripts/aboutscript.js
@@ -6,20 +6,20 @@
         var text = "";
 
         $.when(
-            $.get("Data/marathons.json", function (data) {
-                marathons = data["MarathonList"];
+            $.get('Data/marathons.json', function (data) {
+                marathons = data['MarathonList'];
                 var i = 0;
                 for (i = 0; i < marathons.length; i++) {
-                    text += "Name: " + marathons[i].MarathonName + "\r\nLength: " + marathons[i].Length+"\r\nMovies:\r\n";
+                    text += 'Name: ' + marathons[i].MarathonName + '\r\nLength: ' + marathons[i].Length+'\r\nMovies:\r\n';
                     var j = 0;
                     for (j = 0; j < marathons[i].Movies.length; j++) {
-                        text += marathons[i].Movies[j] + "\r\n";
+                        text += marathons[i].Movies[j] + '\r\n';
                     }
-                    text += "######################################################################################\r\n";
+                    text += '######################################################################################\r\n';
                 }
 
-                text+="*Extended Cut";
-                $(".btndown").attr("href", "data:text/plain;charset=utf-8,"+encodeURIComponent(text));
+                text+='*Extended Cut';
+                $('.btndown').attr('href', 'data:text/plain;charset=utf-8,'+encodeURIComponent(text));
             })
             );
     });
