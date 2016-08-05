2d38058b9e3a9aa6e7121b1c9cc99afba5aee7d1
Knowledgeable Mammoth <T-K-M@users.noreply.github.com>
2016-08-05 11:31:53 +0300

    Update indexscripts.js

--- Scripts/indexscripts.js
+++ Scripts/indexscripts.js
@@ -1,16 +1,16 @@
-﻿"use strict";
+'use strict';
 (function () {
-    var marathons = "";
+    var marathons = '';
     $(document).ready(function () {
-        var evenhtml = "";
-        var oddhtml = "";
+        var evenhtml = '';
+        var oddhtml = '';
 
-        var htmlcode = "";
+        var htmlcode = '';
 
         $.when(
-            $.get("HTML/evenlistitem.html", function (data) { evenhtml = data; }),
-            $.get("HTML/oddlistitem.html", function (data) { oddhtml = data; }),
-            $.get("Data/marathons.json", function (data) { marathons = data["MarathonList"]; })).done(
+            $.get('HTML/evenlistitem.html', function (data) { evenhtml = data; }),
+            $.get('HTML/oddlistitem.html', function (data) { oddhtml = data; }),
+            $.get('Data/marathons.json', function (data) { marathons = data['MarathonList']; })).done(
             function () {
                 var i = 0;
                 for (i = 0; i < marathons.length; i++) {
@@ -29,28 +29,28 @@
 
 
     function createRow(code, index) {
-        var tempcode = "";
-        var spancode = "";
+        var tempcode = '';
+        var spancode = '';
         tempcode = code;
 
         var $tempcode = $(tempcode);
 
-        $tempcode.find(".MarathonName").text(marathons[index].MarathonName);
+        $tempcode.find('.MarathonName').text(marathons[index].MarathonName);
         var i = 0;
         for (i = 1; i < marathons[index].Movies.length; i++) {
-            spancode += "<span>" + marathons[index].Movies[i] + "</span>\n";
+            spancode += '<span>' + marathons[index].Movies[i] + '</span>\n';
 
         }
         if (index % 2 == 0) {
-            $tempcode.find(".dropbtn").text(marathons[index].Movies[0]);
-            $tempcode.find(".content-div").html(spancode);
+            $tempcode.find('.dropbtn').text(marathons[index].Movies[0]);
+            $tempcode.find(.content-div').html(spancode);
         } else {
 
-            $tempcode.find(".odddropbtn").text(marathons[index].Movies[0]);
-            $tempcode.find(".oddcontent-div").html(spancode);
+            $tempcode.find('.odddropbtn').text(marathons[index].Movies[0]);
+            $tempcode.find('.oddcontent-div').html(spancode);
         }
 
-        $tempcode.find(".Length").text(marathons[index].Length);
+        $tempcode.find('.Length').text(marathons[index].Length);
         return $tempcode.html();
     }
 
@@ -63,7 +63,7 @@ function scrollDropDown(a) {
     a.nextElementSibling.classList.toggle('show');
     var h2 = $(document).height();
     if (h2 > h1 && $(window).width() > 800) {
-        $("html, body").animate({ scrollTop: $(document).height() }, 500);
+        $('html, body').animate({ scrollTop: $(document).height() }, 500);
     }
 };
 
